ec81f70127fbeaa0e5c9bfdb41b98043ae4fa091
Raja Babu <iamrajababu123@gmail.com>
2021-01-23 18:09:39 +0000

    Portfolio

--- profile-frontend/src/data/config.js
+++ profile-frontend/src/data/config.js
@@ -1,10 +1,10 @@
 
 module.exports = {
   // TODO: change your profile information here
-  name: "Ashok Kumar",
+  name: "Raja Babu",
   greeting: "Hey 👋",
-  greetingDescription: "I'm Ashok Kumar and I'm a Software Engineer!",
-  githubUrl: "https://github.com/ashokkumar95",
-  linkedinUrl: "https://linkedin.com/ashokkumar1",
-  cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
+  greetingDescription: "I'm Raja Babu and I'm a Software Developer!",
+  githubUrl: "https://github.com/raja-babu",
+  linkedinUrl: "https://www.linkedin.com/in/raja-babu-ju/",
+  cvLink: "https://drive.google.com/file/d/1CPrMHDLHTLi92SQlrzBBMjbC3Bt5hoQ2/view?usp=sharing",
 };
